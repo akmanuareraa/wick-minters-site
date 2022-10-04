1abc207f68b317ff00640b586c1abe0e00a76104
Manuareraa <manuareraa@gmail.com>
2022-10-04 19:13:44 +0530

    Social Links Added

--- src/App.js
+++ src/App.js
@@ -16,6 +16,20 @@ function App() {
           <img src="/assets/9.svg"></img>
           <input className="input absolute input-custom-position bg-white text-black font-semibold text-center"></input>
           <button className="btn absolute button-custom-position text-black bg-gray-400 font-bold hover:bg-slate-500">Subscribe</button>
+          <div className="absolute flex flex-row space-x-6 social-custom-position">
+            <a href="https://twitter.com/WickMinters" className="">
+              <img src="/assets/twitter.svg" className="w-32 h-32"></img>
+            </a>
+            <a href="https://t.me/Wickminters" className="">
+              <img src="/assets/telegram.svg" className="w-32 h-32"></img>
+            </a>
+            <a href="https://discord.gg/kpfaETZTKx" className="">
+              <img src="/assets/discord.svg" className="w-32 h-32"></img>
+            </a>
+            <a href="#" className="">
+              <img src="/assets/medium.svg" className="w-32 h-32"></img>
+            </a>
+          </div>
         </div>
       </div>
     </>
